f20e4b5079e50a1c6ee830dcb00de78a4fdccffd
MrSinaf <mrsinaf@sinafproduction.xyz>
2026-04-21 11:22:33 +0200

    Adding splash screen and UI Menu improvements (ﾉ◕ヮ◕)ﾉ
    > Création d'une scène de splash avec une animation d'ouverture.
    > Intégration et chragement de nouvelles ressources graphiques (`ui.png`, `tommy.ttf`).
    > Mise à jour de la scène `Menu`.

--- BattleshipBasic.Game/Program.cs
+++ BattleshipBasic.Game/Program.cs
@@ -1,5 +1,28 @@
 ﻿using BattleshipBasic.Scenes;
 using Ratelite;
+using Ratelite.Resources;
 using Ratelite.UI;
+using Ratelite.UI.Widgets;
 
-R.CreateGame("Battleship Basic").AddModule<UIModule>().SetStartingScene<Menu>().Run();
+R.CreateGame("Battleship Basic")
+ .AddModule<UIModule>()
+ .LoadingAssets(async progress =>
+ {
+	 await Vault.LoadResource<BitmapFont>("fonts/tommy.ttf", "default");
+	 UIPrefab.Add<Label>(string.Empty, LabelPrefab);
+	 progress.Report(0.5F);
+	 await Vault.LoadResource<Texture2D>("textures/purrvert.png", "purrvert-icon");
+	 progress.Report(0.75F);
+	 await Vault.LoadResource<Texture2D>("textures/ui.png", "ui");
+	 progress.Report(1);
+ })
+ .SetStartingScene<Splash>()
+ .Run();
+
+void LabelPrefab(Label e)
+{
+	var font = Vault.GetAsset<BitmapFont>("default")!;
+	e.font = font.data;
+	e.material = font.material;
+	e.useMeshBoundsSize = false;
+}
--- BattleshipBasic.Game/Scenes/Menu.cs
+++ BattleshipBasic.Game/Scenes/Menu.cs
@@ -1,5 +1,5 @@
-﻿using BattleshipBasic.Core;
-using Ratelite;
+﻿using Ratelite;
+using Ratelite.Resources;
 using Ratelite.UI;
 using Ratelite.UI.Widgets;
 
@@ -8,7 +8,6 @@ namespace BattleshipBasic.Scenes;
 public class Menu : Scene
 {
 	private Canvas canvas = null!;
-	private Label? label;
 	
 	public override void Init()
 	{
@@ -17,13 +16,43 @@ public class Menu : Scene
 	
 	public override void Start()
 	{
-		Client.Connection();
-		Client.onMessageReceived += (id, message) => label?.text += $"\n{id} : {message}";
-		Client.onPlayerJoined += id => label?.text += $"\nPlayer joined : {id}";
-		canvas.root.AddChild(new Button("Say hello", () => Client.SendMessage("Hello!")));
-		canvas.root.AddChild(label = new Label
+		canvas.root.padding = new Region(10);
+		var title = new Layout
 		{
-			pivotAndAnchors = new Vector2(0, 1)
-		});
+			orientation = Orientation.Vertical,
+			pivot = new Vector2(0.5F),
+			anchors = new Vector2(0.5F, 0.75F),
+			spacing = 5,
+			alignment = 1
+		};
+		title.AddChild(
+			new Image(Vault.GetAsset<Texture2D>("ui")!)
+			{
+				scale = new Vector2(8)
+			}
+		);
+		title.AddChild(new Label("Développé par PurrVert"));
+		canvas.root.AddChild(title);
+		
+		var buttons = new Layout
+		{
+			orientation = Orientation.Horizontal,
+			pivotAndAnchors = new Vector2(0.5F, 0),
+			spacing = 10
+		};
+		buttons.AddChild(new Button("Héberger", () => { }));
+		buttons.AddChild(new Button("Rejoindre", () => { }));
+		buttons.AddChild(new Button("Quitter", () => { }));
+		canvas.root.AddChild(buttons);
+		
+		canvas.root.AddChild(
+			new TextInput
+			{
+				placeholder = "Pseudo",
+				value = Environment.GetEnvironmentVariable("USERNAME") ?? string.Empty,
+				pivotAndAnchors = new Vector2(0.5F, 0),
+				position = new Vector2(0, 60)
+			}
+		);
 	}
 }
